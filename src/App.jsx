38766b0dfb91ea59e71e909fe91d3475f8ec8fda
scrpti <pedroscarpati@uma.es>
2024-04-01 19:45:50 +0200

    Diagrama de Requisitos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import MyComponent from './Components/MyComponent';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Bienvenido a BookEZ</h1>
-    </>
-  )
+    <div className='centrar'>
+      <h1>Mi Aplicación React</h1>
+      <MyComponent />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h2>Este es mi componente</h2>
+      <p>Bienvenido a mi aplicación React.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
